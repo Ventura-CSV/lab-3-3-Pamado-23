c450030c26736723638eae9dae290de648acebab
Pamado-23 <pamado1@my.vcccd.edu>
2024-06-27 17:57:58 -0700

    Lab 3-3

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ def main():
     Code Your Program here
     """
 
+    if num1 >= num2 and num1 >= num3:
+        maxnum = num1
+    elif num2 >= num1 and num2 >= num3:
+        maxnum = num2
+    else:
+        maxnum = num3
+
+
     print(f'The greates number is {maxnum}')
     ########################################
     # Do not delete the return statement
